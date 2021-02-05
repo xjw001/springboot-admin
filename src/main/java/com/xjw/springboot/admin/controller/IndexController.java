65c2deb20318f45a6d99f2c29608c20f5ae2f038
xiongjw <xiongjianwen@hrfax.cn>
2021-02-05 13:36:41 +0800

    登录功能

--- src/main/java/com/xjw/springboot/admin/controller/IndexController.java
+++ src/main/java/com/xjw/springboot/admin/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.xjw.springboot.admin.controller;
 import com.xjw.springboot.admin.bean.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,17 +21,27 @@ public class IndexController {
     }
 
     @PostMapping("/login")
-    public String main(User user, HttpSession session){
+    public String main(User user, HttpSession session, Model model){
         if(!ObjectUtils.isEmpty(user.getUserName()) && StringUtils.hasLength(user.getPassword())){
             session.setAttribute("loginUser",user);
+            log.info("登录成功!");
+            return "redirect:/main.html";
+        }else{
+            model.addAttribute("msg","账号或者密码错误");
+            return "login";
         }
-        log.info("登录成功!");
-        return "redirect:/main.html";
     }
 
     @GetMapping("/main.html")
-    public String mainPage(){
-        log.info("访问主页!");
-        return "index";
+    public String mainPage(HttpSession session,Model model){
+        Object loginUser = session.getAttribute("loginUser");
+        if(!ObjectUtils.isEmpty(loginUser)){
+            log.info("访问主页!");
+            return "index";
+        }else{
+            model.addAttribute("msg","请重新登录");
+            return "login";
+        }
+
     }
 }
